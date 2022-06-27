4fb3db8e1ecdd2efdb3deaecd901b56c7ff3e35c
abelagaba <77072924+abelagaba@users.noreply.github.com>
2022-06-27 13:09:19 +0300

    Initial commit

---
+++ app/src/main/java/com/agaba/alibabawebview/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.agaba.alibabawebview
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ImageView
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    Handler().postDelayed({
+        val a = Intent(this@SplashActivity,MainActivity::class.java)
+        startActivity(a)
+        finish()
+    },4000)
+    }
+}
